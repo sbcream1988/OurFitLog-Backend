0cfac4da3afff5e4cde7b8d857202f70378f45b7
sbcream1988 <sbcream1988@gmail.com>
2026-03-18 20:30:04 +0900

    chore: 프로젝트 초기 패키지 구조 생성 및 .gitignore 설정

---
+++ src/main/java/com/ofl/OurFitLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ofl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurFitLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurFitLogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ofl/OurFitLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurFitLogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
